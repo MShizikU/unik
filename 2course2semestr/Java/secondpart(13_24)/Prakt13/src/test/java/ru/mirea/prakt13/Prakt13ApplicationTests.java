44ecc729fe20d694ddf5140d26dbf0aacd81d388
MrShizikU <sidorovstasd7@gmail.com>
2023-04-02 12:49:45 +0300

    Java

---
+++ 2course2semestr/Java/secondpart(13_24)/Prakt13/src/test/java/ru/mirea/prakt13/Prakt13ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mirea.prakt13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prakt13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
