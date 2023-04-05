5858f5c0bb96335f0962f7eb2d1a464b60cfda2e
MShizik <sidorovstasdw@gmail.com>
2023-04-05 21:46:22 +0300

    Java

---
+++ 2course2semestr/Java/secondPart(13_24)/Java_spring_task19/src/test/java/com/example/Java_spring_task19/JavaSpringTask19ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Java_spring_task19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringTask19ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
