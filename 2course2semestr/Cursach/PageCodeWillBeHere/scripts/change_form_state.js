13c07b9431b8f821e9b760157c970021774c2a26
MShizik <sidorovstasdw@gmail.com>
2023-04-18 14:54:49 +0300

    Cursach

--- 2course2semestr/Cursach/PageCodeWillBeHere/scripts/change_form_state.js
+++ 2course2semestr/Cursach/PageCodeWillBeHere/scripts/change_form_state.js
@@ -1,9 +1,30 @@
 const form_title = document.getElementsByClassName('form_title');
 
-const
+const elements = [...document.getElementsByClassName('enabled')].concat([...document.getElementsByClassName('disabled')])
+console.log(elements)
 
+console.log(form_title);
 [...form_title].forEach(function(title) {
-    if (title.innerHTML == 'Регистрация'){
-
-    }
+    title.addEventListener('click', function(){
+        
+        if(window.getComputedStyle(title).getPropertyValue('order') === '2'){
+            active_title = document.querySelector('.active')
+            active_title.classList.remove('active')
+            active_title.classList.add('inactive')
+            title.classList.add('active')
+            title.classList.remove('inactive');
+            [...elements].forEach(function(element){
+                console.log(element.classList)
+                if(element.classList.contains('disabled')){
+                    element.classList.add('enabled')
+                    element.classList.remove('disabled')
+                }else{
+                    element.classList.remove('enabled')
+                    element.classList.add('disabled')
+                }
+                
+            })
+            
+        }
+    })
 })
