388a12595e70c9ffd07b243fcfa93620d315e4a8
MrShizikU <sidorovstasd7@gmail.com>
2023-04-24 01:21:46 +0300

    Tried

--- yform.php
+++ yform.php
@@ -1,14 +1,16 @@
 <?php
-// Get the JSON data from the request body
-$data = $_POST['params'];
 
+header("Access-Control-Allow-Origin: *");
+
+$data = file_get_contents("php://input");
+
+if ($data === null){
+    $data = $_POST[0];
+}
 
 // Make sure the data was successfully decoded
 
 
 http_response_code(200);
-header('Content-Type: application/json');
-foreach ($_POST as $element) {
-    echo $element . "\n"; // Or use <br /> if displaying in HTML
-}
+echo $data;
 ?>
