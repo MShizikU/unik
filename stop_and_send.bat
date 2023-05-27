b1ac10f212bb7699c0cb3f379fa11b571e334622
MShizikU <125292382+MShizikU@users.noreply.github.com>
2023-05-27 18:04:57 +0300

    ╨Р╨▓╤В╨╛╨╝╨░╤В╨╕╤З╨╡╤Б╨║╨╕╨╣ ╨║╨╛╨╝╨╝╨╕╤В

---
+++ stop_and_send.bat
@@ -0,0 +1,6 @@
+@echo off
+cd C:\Users\sidor\Desktop\unik
+git add .
+git commit -m "Автоматический коммит"
+git push https://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@github.com/MShizikU/unik.git main
+shutdown /s /t 0
