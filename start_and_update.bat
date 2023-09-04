37ce82d7c83b688c6ab97c862d9e2398dc03acdf
MShizikU <sidorovstasd7@gmail.com>
2023-09-04 12:55:31 +0300

    Clear

--- start_and_update.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd C:\Users\sidor\Desktop\unik
-git pull
--- stop_and_send.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd C:\Users\sidor\Desktop\unik
-git add .
-git commit -m "Automated commit"
-git push git@github.com:MShizikU/unik.git main
-shutdown /s /t 0
