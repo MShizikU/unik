1f32cc0b7bc55733fd6a571dcca0417b763225b6
MShizikU <sidorovstasd7@gmail.com>
2023-10-28 21:26:41 +0300

    Interfaces 5,6

--- 3course1semestr/Interfaces/Pr5/api/user.php
+++ 3course1semestr/Interfaces/Pr5/api/user.php
@@ -6,18 +6,19 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$mysql = mysqli_connect("db", "user", "password", "appDB");
+
+$mysql = mysqli_connect("localhost", "sidoroua_interfa", "P@ssw0rd", "sidoroua_interfa");
 $result = array();
 
 if ($mysql){
     if ($_SERVER['REQUEST_METHOD'] == 'GET'){
         parse_str($_SERVER['QUERY_STRING'], $data);
         if (!array_key_exists('id', $data)){
-            $resDB = mysqli_query($mysql, 'SELECT * FROM note');
+            $resDB = mysqli_query($mysql, 'SELECT * FROM users');
             if ($resDB != false){
                 $result['result'] = 'Success';
                 foreach($resDB as $row){
-                    $result['items'][] = array("id" => $row['id'], "fullname" => $row['fullname'], "phone" => $row['phone'], "description" => $row['description']);
+                    $result['items'][] = array("id" => $row['id'], "fullname" => $row['fullname']);
                 }
             }
             else{
@@ -25,63 +26,53 @@ if ($mysql){
                 $result['message'] = mysqli_error($mysql);
             }
         }else{
-            $resDB = mysqli_query($mysql, "SELECT * FROM note WHERE `id` = '" . $data['id'] . "'");
+            $resDB = mysqli_query($mysql, "SELECT * FROM users WHERE `id` = '" . $data['id'] . "'");
             if ($resDB != false){
                 $result['result'] = 'Success';
                 foreach($resDB as $row){
-                    $result['items'][] = array("id" => $row['id'], "fullname" => $row['fullname'], "phone" => $row['phone'], "description" => $row['description']);
+                    $result['items'][] = array("id" => $row['id'], "fullname" => $row['fullname']);
                 }
             }
             else{
                 $result['result'] = "Database error";
-                $result['data'] = $data;
                 $result['message'] = mysqli_error($mysql);
             }
         }
     }
     else if ($_SERVER['REQUEST_METHOD'] == 'POST'){
         $data = $_POST;
-        if ($data['fullname'] != null && $data['phone'] != null && $data['description'] != null){
-            $resDB = mysqli_query($mysql, "INSERT INTO note(fullname,phone,description) VALUES ('" . $data['fullname'] . "','" . $data['phone'] . "','" . $data['description'] . "')");
+        if ($data['fullname'] != null){
+            $resDB = mysqli_query($mysql, "INSERT INTO users(fullname) VALUES ('" . $data['fullname'] . "')");
             $result['result'] = $resDB;
         }else{
             $result['result'] = "Request error";
-            $result['data'] = $data;
             $result['message'] = "Wrong passed data";
         }
     }
     else if ($_SERVER['REQUEST_METHOD'] == 'PUT'){
         parse_str(file_get_contents("php://input"),$data);
-        if ($data['fullname'] != null && $data['id'] != null && $data['phone'] != null && $data['description'] != null){
-            $resDB = mysqli_query($mysql, "UPDATE note SET `fullname` = '" . $data['fullname'] . "' WHERE `id` = '" . $data['id'] . "'");
-            if ($resDB)
-                $resDB = mysqli_query($mysql, "UPDATE note SET `phone` = '" . $data['phone'] . "' WHERE `id` = '" . $data['id'] . "'");
-            if ($resDB)
-                $resDB = mysqli_query($mysql, "UPDATE note SET `description` = '" . $data['description'] . "' WHERE `id` = '" . $data['id'] . "'");
+        if ($data['fullname'] != null && $data['id'] != null){
+            $resDB = mysqli_query($mysql, "UPDATE users SET `fullname` = '" . $data['fullname'] . "' WHERE `id` = '" . $data['id'] . "'");
             $result['result'] = $resDB;
-            if (!$resDB){
-                $result['error'] = mysqli_error($mysql);
-            }
         }else{
             $result['result'] = "Request error";
-            $result['data'] = $data;
             $result['message'] = "Wrong passed data";
         }
     }
     else if ($_SERVER['REQUEST_METHOD'] == 'DELETE'){
         parse_str(file_get_contents("php://input"),$data);
         if ($data['id'] != null){
-            $resDB = mysqli_query($mysql, "DELETE FROM note WHERE `id` = '" . $data['id'] . "'");
+            $resDB = mysqli_query($mysql, "DELETE FROM users WHERE `id` = '" . $data['id'] . "'");
             $result['result'] = $resDB;
         }else{
             $result['result'] = "Request error";
-            $result['data'] = $data;
             $result['message'] = "Wrong passed data";
         }
     }
 }
 else{
     $result['result'] = "Database error";
+    $result['message'] = mysqli_error($mysql);
 }
 
 echo json_encode($result);
