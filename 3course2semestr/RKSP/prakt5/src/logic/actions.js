0b23d03a9095ac359db0ca26ff2ac9c10f59555e
Sidorov Stas <SSidorov@croc.ru>
2024-09-15 19:45:14 +0300

    TVKP

--- 3course2semestr/RKSP/prakt5/src/logic/actions.js
+++
@@ -1,3 +0,0 @@
-export const toggleAgreement = () => ({
-    type: 'TOGGLE_AGREEMENT'
-});
--- 3course2semestr/TOT/prakt8/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE example (
-    id SERIAL PRIMARY KEY,
-    value VARCHAR(30)
-);
-
-INSERT INTO example (value) VALUES
-('One'),
-('Two'),
-('Three');
